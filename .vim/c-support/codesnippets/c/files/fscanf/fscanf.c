b22d73146ac264d4528f73bd986f9bd867bf985c
root <root@li377-11.members.linode.com>
2012-04-04 23:17:06 -0400

    mv files to files folder

---
+++ .vim/c-support/codesnippets/c/files/fscanf/fscanf.c
@@ -0,0 +1,28 @@
+#include	<errno.h>
+#include	<math.h>  
+#include	<stdio.h> 
+#include	<stdlib.h>
+#include	<string.h>
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  main
+ *  Description:  main function
+ * =====================================================================================
+ */
+	int
+main ( int argc, char *argv[] )
+{
+    char a[10] = {0}, b[10] = {0};
+    int i;
+    FILE *fp;
+    fp=fopen("test.db","r");
+    if(fp == NULL)
+        exit(1);
+    fscanf(fp, "%s", a);
+    fscanf(fp,"%s", b);
+    printf("%s\n%s\n", a, b);
+    fclose(fp);
+	return EXIT_SUCCESS;
+}		/* ----------  end of function main  ---------- */
+
